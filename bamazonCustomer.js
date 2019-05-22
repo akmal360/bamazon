5fc7a21d2565a110e016d6f9b48369afadbd7194
Md Akmal Hossain <akmal2080@gmail.com>
2019-05-22 14:54:59 -0400

    final

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,7 +10,7 @@ var connection = mysql.createConnection({
     port: 3306,
     user: "root",
     password: "", //password
-    database: "bamazonDB"
+    database: "bamazon"
 });
 
 //Connect to the mysql server and sql database
